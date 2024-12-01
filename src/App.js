2260769e43d29a8a44078e82d5c8027cd76a1b27
juan-suarez <54161893+juan-suarez@users.noreply.github.com>
2024-11-30 23:14:55 -0500

    Routing (#1)
    * routes configured
    
    * protected routes component added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={
+            <ProtectedRoute>
+              home
+            </ProtectedRoute>
+          } />
+          <Route path="/bingo" element={
+            <ProtectedRoute>
+              <h1>bingo</h1>
+            </ProtectedRoute>
+          } />
+          <Route path="/login" element={<h1>login</h1>} />
+          <Route path="/register" element={<h1>register</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
