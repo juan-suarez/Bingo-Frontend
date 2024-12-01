ada8b05cb5b9d174fec1a974a5d09083843cdc76
juan-suarez <juan.suarez1@utp.edu.co>
2024-11-30 22:44:30 -0500

    routes configured

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={"Home"} />
+          <Route path="/login" element={<h1>login</h1>} />
+          <Route path="/register" component={<h1>register</h1>} />
+          <Route path="/bingo" component={<h1>bingo</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
