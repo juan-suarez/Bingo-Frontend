257bcc1ee99cf06be6af336f2db2746980072daa
juan-suarez <juan.suarez1@utp.edu.co>
2024-12-01 10:54:54 -0500

    rebuild and register component

--- src/components/RegisterFrom.jsx
+++ src/components/RegisterFrom.jsx
@@ -2,16 +2,16 @@ import { useState } from "react"
 import { loger } from "../gateway/auth";
 import { useNavigate } from "react-router-dom";
 
-export const LoginForm = () => {
+export const RegisterForm = () => {
   const [credentials, setCredentials] = useState({areValidCredentials:true});
   const navigate = useNavigate();
 
   const handleSubmit = async(e) => {
     e.preventDefault();
-    const { email, password} = credentials;
+    const { email, password, userName} = credentials;
     try{
       const response = await loger(email,password);
-      if( response.status === 200){
+      if( response.status == 200){
         navigate('/');
         return;
       }
@@ -27,13 +27,25 @@ export const LoginForm = () => {
   return (
     <div className="rounded-3xl bg-white shadow-xl w-full md:w-1/2 lg:w-2/5 h-3/4 border flex justify-center items-center p-4">
       <form className="w-full lg:w-1/2" id="form" onSubmit={handleSubmit}>
-        <h1 className="text-center text-3xl font-serif font-bold">Login</h1>
+        <h1 className="text-center text-3xl font-serif font-bold">Registro</h1>
+        <h5 className="text-sm text-center">crea tu cuenta, ahora!</h5>
         {
           !credentials.areValidCredentials?
           <h1 className="text-center text-red-600">{credentials.message}</h1>:
           null
         }
         <div className="mt-12 mb-4 flex flex-col text-sm">
+          <label className="" htmlFor="">usuario</label>
+          <input 
+            className="w-full border rounded-lg p-2" 
+            type="text" 
+            placeholder="BingoKing"
+            value={credentials.email}
+            onChange={(e)=>{setCredentials({...credentials, email: e.target.value})}}
+            required
+          />
+        </div>
+        <div className="mb-4 flex flex-col text-sm">
           <label className="" htmlFor="">Correo</label>
           <input 
             className="w-full border rounded-lg p-2" 
@@ -44,7 +56,7 @@ export const LoginForm = () => {
             required
           />
         </div>
-        <div className="mb-4 flex flex-col text-sm">
+        <div className="mb-12 flex flex-col text-sm">
           <label className="" htmlFor="">Contraseña</label>
           <input 
             className="w-full border rounded-lg p-2" 
@@ -55,18 +67,7 @@ export const LoginForm = () => {
             required
           />
         </div>
-        <div className="flex justify-between mb-4 text-xs">
-          <div className="flex">
-            <input className="me-2" type="checkbox" />
-            <h5>Recuerdame</h5>
-          </div>
-          <a className= "underline decoration-sky-500 text-sky-500" href=""> ¿olvidates la contraseña?</a>
-        </div>
-        <button className="bg-sky-500 rounded-lg p-1 mb-12 w-full text-center text-white" type="submit">Ingresar</button>
-        <div className="flex justify-between text-xs">
-          <h5 className="text-slate-500 ">¿Aun no tienes una cuenta?</h5>
-          <a className= "underline decoration-sky-500" href="/register"> Registrate</a>
-        </div>
+        <button className="bg-sky-500 rounded-lg p-1 mb-12 w-full text-center text-white" type="submit">Crear cuenta</button>
       </form>
     </div>
   )
---
+++ src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from "../components/RegisterFrom"
+
+export const Register = ()=> {
+
+  return (
+    <div className=" bg-neutral-200 h-screen w-screen flex justify-center items-center p-6 p-12 font-sans">
+      <RegisterForm />
+    </div>
+  )
+} 
