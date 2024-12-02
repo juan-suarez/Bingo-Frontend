b7bd3014ca0adda903aa0855cef0448bd1a4d9c8
juan-suarez <juan.suarez1@utp.edu.co>
2024-12-01 20:46:13 -0500

    time to start timer implemented

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,9 +2,10 @@ import { CalledNumbers } from "../components/CalledNumbers"
 import { LastResults } from "../components/LastResults"
 
 export const Game = () => {
-  const calledNumbers = [1,25]
+  const calledNumbers = [1,25];
+  const counter = '00:55'
   return (
-    <div className="w-screen h-screen grid grid-cols-4 gap-4 p-2">
+    <div className="w-screen h-screen grid grid-cols-4 gap-4 p-2  bg-gradient-to-r from-indigo-700 to-sky-600">
       <div className="grid grid-rows-4 gap-1 p-1 pt-2">
         <div className=" ">
           <LastResults numbers={calledNumbers}/>
@@ -14,8 +15,18 @@ export const Game = () => {
         </div>
       </div>
       <div className="col-span-2 border-2 border-black grid grid-rows-6 gap-1 p-1">
-        <div className="border-2 border-black ">
-          counter
+        <div className="flex flex-col justify-center items-center text-white">
+          {
+            counter === '00:00'?
+            <div>
+              <h5 className="text-3xl font-bold"> Juego en curso! </h5>
+            </div>
+            :
+            <div className="text-center">
+              <h5 className="text-lg">Tiempo para inicar</h5>
+              <h5 className="text-5xl font-bold"> {counter} </h5>
+            </div>
+          }
         </div>
         <div className="border-2 border-black row-span-4">
           player board
